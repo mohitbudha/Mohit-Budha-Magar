c58a54816932a8aa4c2df5f792d66be650a0d54b
mohitbudha <mohitbudhamagar8@gmail.com>
2026-05-09 15:44:38 +0545

    1st

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://my-portfolio-backend-1-db8u.onrender.com/api"
+  baseURL: "https://my-portfolio-backend-1-93iz.onrender.com/api"
 });
 
 api.interceptors.request.use((config) => {
